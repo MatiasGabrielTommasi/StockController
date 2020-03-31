e40941af066e06db1835777353b4a7b0f89cc912
MatiasGabrielTommasi <56137800+MatiasGabrielTommasi@users.noreply.github.com>
2020-03-31 18:47:35 -0300

    Actualizacion, se definieron los metodos

--- src/main/java/ar/com/playmedia/controller/ProductC.java
+++ src/main/java/ar/com/playmedia/controller/ProductC.java
@@ -12,9 +12,36 @@ public class ProductC{
     public void saveProduct(ProductM oProduct){
     }
 
-    public void updateProduct(ProductM oProduct){
+    public void searchProduct(ProductM oProduct){
     }
 
     public void deleteProduct(ProductM oProduct){
+    }
+
+    public void getIntId(ProductM oProduct){
+    }
+
+    public void setProductName(ProductM oProduct){
+    }
+
+    public void getProductName(ProductM oProduct){
+    }
+
+    public void setPrice(ProductM oProduct){
+    }
+
+    public void getPrice(ProductM oProduct){
+    }
+
+    public void setStock(ProductM oProduct){
+    }
+
+    public void getStock(ProductM oProduct){
+    }
+
+    public void setCode(ProductM oProduct){
+    }
+
+    public void getCode(ProductM oProduct){
     }
 }
