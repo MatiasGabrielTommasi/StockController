97e2860a368309633d8ffe1cbc987801bdc6e94b
RafaelBorda <58663277+RafaelBorda@users.noreply.github.com>
2020-04-10 22:35:05 -0300

    Se sube PersonC.java

---
+++ src/main/java/ar/com/playmedia/controller/PersonC.java
@@ -0,0 +1,22 @@
+package ar.com.playmedia.controller;
+
+import ar.com.playmedia.model.PersonM;
+
+public class PersonC{
+
+    public void listPerson(){
+        
+    }
+
+    public void savePerson(){
+            
+    }
+
+    public void updatePerson(){
+
+    }
+
+    public void deletePerson(){
+
+    }
+}
