3e34a4a3ca97365fb2952484ec7fb2e02c31d2f3
RafaelBorda <58663277+RafaelBorda@users.noreply.github.com>
2020-04-11 10:23:57 -0300

    PersonC.java

---
+++ src/main/java/ar/com/playmedia/controller/PersonC.java
@@ -0,0 +1,22 @@
+package ar.com.playmedia.controller;
+
+// import java.util.ArrayList;
+// import ar.com.playmedia.model.PersonM;
+
+public class PersonC{
+    // public ArrayList<PersonM> listPerson(){
+
+    // }
+
+    // public void savePerson(){
+
+    // }
+
+    // public void updatePerson(){
+
+    // }
+
+    // public void deletePerson(){
+
+    // }
+}
